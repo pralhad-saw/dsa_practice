085bf77b79aabc36cbdb20de0c8e43078d59be60
pralhad-saw <shubhamsav109@gmail.com>
2026-09-15 14:04:19 +0530

    Comment out complexity table in sumofdigits.py
    Commented out the complexity table for various problems.

--- sumofdigits.py
+++ sumofdigits.py
@@ -8,3 +8,13 @@ def sum_of_digits(number: int) -> int:
 # Sum of digits
 print(sum_of_digits(12345))    # 15
 print(sum_of_digits(-987))     # 24
+
+
+# #| Problem               | Time complexity              | Space complexity |
+# | --------------------- | ---------------------------- | ---------------- |
+# | Armstrong number      | O(d)O(d)O(d)                 | O(d)O(d)O(d)     |
+# | Perfect number        | O(n)O(\\sqrt{n})O(n​)        | O(1)O(1)O(1)     |
+# | Anagram               | O(nlog⁡n)O(n \\log n)O(nlogn) | O(n)O(n)O(n)     |
+# | Vowels and consonants | O(n)O(n)O(n)                 | O(1)O(1)O(1)     |
+# | Sum of digits         | O(d)O(d)O(d)                 | O(d)O(d)O(d)     |
+
