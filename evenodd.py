c66c0d881ad197cb3f77ba7622dfe86c33222bbf
pralhad-saw <shubhamsav109@gmail.com>
2026-09-20 12:47:50 +0530

    Update evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,1 +1,12 @@
 
+n = int(input("Enter the limit: "))
+
+print("Even numbers:")
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+print("\nOdd numbers:")
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        print(i, end=" ")
