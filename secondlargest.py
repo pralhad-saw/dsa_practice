e85fbd5d2a5baffa02317c12acd36701cd2194e6
pralhad-saw <shubhamsav109@gmail.com>
2026-09-20 12:48:07 +0530

    Update secondlargest.py

--- secondlargest.py
+++ secondlargest.py
@@ -1,1 +1,10 @@
 
+arr = list(map(int, input("Enter array elements: ").split()))
+
+unique_elements = list(set(arr))
+
+if len(unique_elements) < 2:
+    print("Second largest element does not exist")
+else:
+    unique_elements.sort(reverse=True)
+    print("Second largest element:", unique_elements[1])
